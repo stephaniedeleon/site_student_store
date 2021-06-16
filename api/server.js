d0a5c07b312f22635ffb0e18b0fe8079b4a890a9
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-16 14:38:28 -0700

    error handling

--- api/server.js
+++ api/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
+const { NotFoundError } = require("./utils/errors")
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+
+
+// Handle all 404 errors that weren't matched by a route
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+});
+
+// Generic error handler - anything that is unhandled will be handled here
+app.use((err, req, res, next) => {
+
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
+
 
 const port = 3001;
 app.listen(port, () => {
