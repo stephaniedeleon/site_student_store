fc49487aa5af009ec91136134b8a74ea55ca62f5
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-17 15:16:42 -0700

    Added about section

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import StoreProducts from "../StoreProducts/StoreProducts"
+import About from "../About/About"
 import "./Home.css"
 
 export default function Home({ products }) {
 
   return (
-    <div className="Home">
-        <h2>Products</h2>
-        <StoreProducts products={products} />
+    <div className="Home">        
+      <h2>About</h2>
+      <About />
+      <h2>Products</h2>
+      <StoreProducts products={products} />
     </div>
   )
 }
