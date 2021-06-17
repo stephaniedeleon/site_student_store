35a51ed0f5a18231cc0050b558a669e1814c56eb
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-17 00:17:46 -0700

    added cors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const storeRouter = require("./routes/store");
 const { NotFoundError } = require("./utils/errors")
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cors());
 
 app.use("/store", storeRouter);
 
